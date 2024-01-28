db4fdcb2069be0a98f53bad14d3c78d2587b1c79
ResoluteLemon <43917540+ResoluteLemon@users.noreply.github.com>
2024-01-29 00:21:49 +0800

    Refactor playRound function
    Ties were incorrectly reported and round incrementer was incrementing
    right before the game decides if the max rounds have been reached,
    causing game to end one round earlier. This commit fixes these bugs.

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -2,7 +2,7 @@ const choices = ['rock', 'paper', 'scissors'];
 const outcomes = ['tie', 'lose', 'win'];
 
 function getComputerChoice() {
-    return Math.floor(Math.random()*3);    
+    return Math.floor(Math.random()*3);
 } 
 
 function getPlayerChoice() {
@@ -27,42 +27,66 @@ function playerFeedback(outcomeText, playerSelection, computerSelection, state) 
     //might take the state as an argument if needed in the future
     switch (outcomeText) {
         case 'win':
-            alert(`You win the round!
-            ${renderGameStats(state, playerSelection, computerSelection)}`);
+            alert(`You win the round! ${choices[playerSelection]} beats ${choices[computerSelection]} ${renderGameStats(state)}`);
             break;
         case 'lose':
-            alert(`You lose the round!
-            ${renderGameStats(state, playerSelection, computerSelection)}`);
+            alert(`You lose the round! ${choices[computerSelection]} beats ${choices[playerSelection]} ${renderGameStats(state)}`);
             break;
         case 'tie':
-            alert(`It is a tie.
-            ${renderGameStats(state, playerSelection, computerSelection)}`);
+            alert(`It is a tie. We'll go again. ${renderGameStats(state)}`);
             break;
         case 'victory':
-            alert(`You win the game!
-            ${renderGameStats(state, playerSelection, computerSelection)}`);
+            alert(`You win the game! ${choices[playerSelection]} beats ${choices[computerSelection]} ${renderGameStats(state)}`);
             break;
         case 'defeat':
-            alert(`You lose the game!
-            ${renderGameStats(state, playerSelection, computerSelection)}`);
+            alert(`You lose the game! ${choices[computerSelection]} beats ${choices[playerSelection]} ${renderGameStats(state)}`);
             break;
     }
 }
 
-function renderGameStats (state, playerSelection, computerSelection) {
-    return `${choices[playerSelection]} beats ${choices[computerSelection]}
-    Round: ${state.round} of ${state.maxRounds}
+function renderGameStats (state) {
+    return `
+    Round: ${state.currentRound} of ${state.maxRounds}
     Player Score: ${state.playerScore}
     Computer Score: ${state.computerScore}`;
 }
+
+function resolveGameWinner(state, playerSelection, computerSelection) {
+    return state.playerScore > state.computerScore ? 
+    playerFeedback('victory', playerSelection, computerSelection, state) : playerFeedback('defeat', playerSelection, computerSelection, state);
+}
+
 
 function playRound(state) {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
     const outcomeText = calculateOutcome(playerSelection, computerSelection);
-    //to remove
-    logRoundStats(playerSelection, computerSelection, outcomeText);
+    if (outcomeText === 'win') {
+        state.playerScore++;
+        playerFeedback(outcomeText,playerSelection,computerSelection,state);
+        return state.currentRound < state.maxRounds ? 
+            (state.currentRound++, playRound(state)) : 
+            resolveGameWinner(state, playerSelection, computerSelection);
+    } else if (outcomeText === 'tie') {
+        playerFeedback(outcomeText,playerSelection,computerSelection,state);
+        return playRound(state);
+    } else {
+        state.computerScore++;
+        playerFeedback(outcomeText,playerSelection,computerSelection,state);
+        return state.currentRound < state.maxRounds ? 
+            (state.currentRound++, playRound(state)) : 
+            resolveGameWinner(state, playerSelection, computerSelection);
+    }
 }
+
+
+const state = {
+    playerScore: 0,
+    computerScore: 0,
+    currentRound: 1,
+    maxRounds: 5
+}
+playRound(state);
 
 function game(maxRounds) {
     //initiate state
@@ -79,12 +103,5 @@ function game(maxRounds) {
     //return game win/lose outcome
 }
 
-//to remove
-function logRoundStats(playerSelection, computerSelection, outcomeText) {
-    console.log(`player chose ${choices[playerSelection]}`);
-    console.log(`computer chose ${choices[computerSelection]}`);
-    console.log(`You ${outcomeText}`);
-}
 
-// playRound(getPlayerChoice, getComputerChoice);
 
